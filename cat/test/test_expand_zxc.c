e810d3c40782d7c283aef1f2b312331f78c4b6fa
hellobertrand <5901952+hellobertrand@users.noreply.github.com>
2026-04-27 10:33:30 +0200

    Add tests for zxc support in bsdcat
    Adds test cases and reference files to verify that bsdcat correctly handles zxc-compressed files, including both empty files and those with content.

---
+++ cat/test/test_expand_zxc.c
@@ -0,0 +1,23 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2026 Bertrand Lebonnois
+ * All rights reserved.
+ */
+#include "test.h"
+
+DEFINE_TEST(test_expand_zxc)
+{
+	const char *reffile = "test_expand.zxc";
+	int f;
+
+	extract_reference_file(reffile);
+	f = systemf("%s %s >test.out 2>test.err", testprog, reffile);
+	if (f == 0 || canZxc()) {
+		assertEqualInt(0, f);
+		assertTextFileContents("contents of test_expand.zxc.\n", "test.out");
+		assertEmptyFile("test.err");
+	} else {
+		skipping("It seems zxc is not supported on this platform");
+	}
+}
